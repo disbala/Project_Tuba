d80c945c9c3ea4d5a542318bd47145ef0db35726
disbala <cort@live.ca>
2014-03-10 13:56:41 -0400

    Revert "made some clouds"
    This reverts commit bd4b84197095173210efae5158ba1585b40cb580.

--- project-tuba/src/com/me/projecttuba/Player.java
+++ project-tuba/src/com/me/projecttuba/Player.java
@@ -22,81 +22,58 @@ public class Player {
 	private Sprite sprite;
 
 	boolean canJump;
-	float gravity;
 
 	Player() {
 		initGraphics();
 		canJump = true;
-		x = 300;
 	}
 
 	public void jump() {
 		vy += 30;
-		if (vy > 30){
-			vy = 30;
-		}
 		canJump = false;
 	}
 
 	public void moveRight() {
 		x += 10;
 	}
-
 	public void moveLeft() {
 		x -= 10;
 	}
 
 	public void initGraphics() {
 		texture = new Texture(Gdx.files.internal("data/player.png"));
-		//		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+//		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
 		TextureRegion region = new TextureRegion(texture, 0, 0, 50, 100);
-
+		
 		sprite = new Sprite(region);
 		sprite.setSize(50, 100);
+//		sprite.setPosition(200, 200);
 
-	}
 
-	public boolean stand(Cloud cloud){
-		
-		if (Math.abs((x+25) - (cloud.x+Cloud.w/2)) < (Cloud.w/2+25) && Math.abs(y - cloud.y) < 15){
-			y = cloud.y;
-			vy = 0;
-			gravity = 0;
-			canJump = true;
-			return true;
-		} else {
-			gravity = 3;
-			return false;
-		}
-		
 	}
 	
-	public void update() {
+	public void update(){
+		int gravity = 3;
+		
 		x += vx;
 		y += vy;
-
+		
 		sprite.setPosition(x, y);
-
-		if (y > 0) {
-			vy -= gravity/4;
-			y -= gravity*4;
+		
+		if (y > 0){
+			vy -= gravity;
 		} else {
 			y = 0;
 			vy = 0;
 			canJump = true;
 		}
 		
-		for (Cloud c : ProjectTuba.clouds){
-			stand(c);
-		}
+		System.out.println(x+","+y);
 		
-		//		System.out.println(x+","+y);
-		//		System.out.println(TimeUtils.nanoTime());
-
 	}
-	
-	public void render(SpriteBatch batch) {
 
+	public void render(SpriteBatch batch) {
+		
 		sprite.draw(batch);
 
 	}
